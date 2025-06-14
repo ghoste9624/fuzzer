58ff3667d5026c796293cb824a1060201f8aa011
ghoste9624 <jackspade44@outlook.com>
2025-06-14 10:52:27 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,4 @@
+import os
+os.system("curl -OL https://raw.githubusercontent.com/emadshanab/LFI-Payload-List/master/LFI%20payloads.txt")
+os.system("wget https://raw.githubusercontent.com/Bo0oM/fuzz.txt/refs/heads/master/fuzz.txt")
+os.system("wget https://raw.githubusercontent.com/osamahamad/FUZZING/refs/heads/main/full_fuzz.txt")        
